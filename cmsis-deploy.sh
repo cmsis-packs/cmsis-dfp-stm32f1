88793a56b896e85cb31164e2ecbb19c84df377c2
Rafael Silva <silvagracarafael@gmail.com>
2021-02-02 23:24:48 +0000

    v4.3.1

--- cmsis-deploy.sh
+++ cmsis-deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-name=cmsis-dfp-template
-vendor=template
-version=0.0
-source_url=
+name=cmsis-dfp-stm32f1
+vendor=Atmel
+version=4.3.1
+source_url=https://github.com/STMicroelectronics/cmsis_device_f1
+commit=2948138428461c0621fd53b269862c6e6bb043ce
 
 build_dir='cmsis_build'
 deploy_dir='cmsis_deploy'
@@ -45,20 +46,23 @@ prepare() {
 
 download() {
     echo "downloading..."
-    curl -o $build_dir/pack-src.pack $source_url
+    git clone $source_url $build_dir/temp
+    cd $build_dir/temp
+    git checkout $commit
+    cd ../..
 }
 
 extract() {
     echo "extracting..."
-    unzip $build_dir/pack-src.pack -d $build_dir
+    mv $build_dir/temp/* $build_dir/
+    rm -rf $build_dir/temp/
 }
 
 deploy() {
     echo "deploying..."
-    cp -r $build_dir/Device $deploy_dir
-    cp -r $build_dir/Documents $deploy_dir
-    cp -r $build_dir/Flash $deploy_dir
-    cp -r $build_dir/SVD $deploy_dir
+    cp -r $build_dir/Include $deploy_dir
+    cp -r $build_dir/Source $deploy_dir
+    cp -r $build_dir/License.md $deploy_dir
 }
 
 prepare
